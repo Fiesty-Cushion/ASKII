425b413754604dedb7636b5f494b70a4e1873254
Fiesty_Cushion <neupane.braz07@gmail.com>
2023-04-09 12:02:30 +0545

    Added function declaration

--- image2ascii.h
+++ image2ascii.h
@@ -1,6 +1,13 @@
-#pragma once
+#ifndef IMAGE2ASCII_H
+#define IMAGE2ASCII_H
+
 #include <vector>
 #include <string>
 #include <opencv2/opencv.hpp>
 
 std::vector<std::string> image2ascii(cv::Mat image);
+
+#endif // !IMAGE2ASCII_H
+
+
+
